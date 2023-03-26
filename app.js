93d79216f3a4a51986d5e184d07f29c3e5920b6c
Okeke Chigozie <81691598+bertinchigozie@users.noreply.github.com>
2023-03-26 13:20:55 +0100

    feat:adjusted setheader in app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "*");
 
   next();
 });
